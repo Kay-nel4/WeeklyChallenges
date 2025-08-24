7e5e1a07839b9087424f400588d178aca3691ed1
Kayla Knelle <kaynell3234@gmail.com>
2025-08-24 17:17:48 -0400

    Completed set 2 of the Weekly Challenges.

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +8,118 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            return char.IsLetter(c);
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            if (vals.Length % 2 == 0)
+            {
+                return true;
+            }
+            return false;
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+
+            return false;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            if (num % 2 != 0)
+            {
+                return true;
+            }
+
+            return false;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            //Was wondering why this won't pass all the tests
+            //var sum = numbers.Sum();
+            //return sum;
+
+            if (numbers == null || numbers.Count() == 0)
+            {
+                return 0;
+            }
+
+            return numbers.Min() + numbers.Max();
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            int stringLength = str1.Length;
+            int stringLengthTwo = str2.Length;
+            if (stringLength < stringLengthTwo)
+            {
+                return stringLength;
+            }
+            return stringLengthTwo;
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+            var sum = numbers.Sum();
+            return sum;
+            
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+            var filteredNumbers = numbers.Where(n => n % 2 == 0).ToArray();
+            return filteredNumbers.Sum();
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return false;
+            }
+            return numbers.Sum() % 2 != 0;
+            //My first attempt I couldn't tell which tests it wasn't passing.
+            /*int numSum = numbers.Sum();
+            if (numSum % 2 != 0 || numSum == null)
+            {
+                return true;
+            }
+            return false;*/
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            if (number <= 0)
+            {
+                return 0;
+            }
+            
+            return number / 2;
+            
+                
+            
+            //Attempt before watching exercise.
+           /* for (int i = 0; i < number; i++)
+            {
+                return number.ToString().Count();
+            }
+            return 0;*/
+            
         }
     }
 }
